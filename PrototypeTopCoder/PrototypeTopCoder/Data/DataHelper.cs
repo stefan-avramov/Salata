207d24889c7ca5be4ea31889ca6446e229796a9f
Stefan Avramov <st3f.1p@gmail.com>
2013-02-15 01:14:26 +0200

    init commit

---
+++ PrototypeTopCoder/PrototypeTopCoder/Data/DataHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrototypeTopCoder
+{
+	public static class DataHelper
+	{
+		public static void RegisterUser(string username, string password, string email)
+		{
+			using (TopCoderPrototypeEntities model = new TopCoderPrototypeEntities())
+			{
+				User user = new User();
+				user.Username = username;
+				user.Password = password;
+				user.Email = email;
+				user.Type = 1;
+				model.Users.AddObject(user);
+				model.SaveChanges();
+			}
+		}
+
+		public static bool UserExists(string username, string password)
+		{
+			using (TopCoderPrototypeEntities model = new TopCoderPrototypeEntities())
+			{
+				return model.Users.Where(x => x.Username == username && x.Password == password).Count() == 1;
+			}
+		}
+	}
+}
