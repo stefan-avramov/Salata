f01bef65c19122ec272489b20fcf0d7ae7190114
Stefan Avramov <st3f.1p@gmail.com>
2013-03-11 07:57:12 +0200

    view upcoming competitions

--- PrototypeTopCoder/PrototypeTopCoder/Controllers/AdminController.cs
+++ PrototypeTopCoder/PrototypeTopCoder/Controllers/AdminController.cs
@@ -1,46 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using PrototypeTopCoder.Models;
 
 namespace PrototypeTopCoder.Controllers
 {
-    public class AdminController : Controller
-    {
-        //
-        // GET: /Competitions/
+	public class AdminController : Controller
+	{
+		//
+		// GET: /Competitions/
 
-        public ActionResult Index()
-        {
-            return View(DataHelper.GetAllCompetitions());
-        }
+		public ActionResult Index()
+		{
+			return View(DataHelper.GetAllCompetitions());
+		}
 
-        public ActionResult CreateCompetition()
-        {
+		public ActionResult CreateCompetition()
+		{
 			ViewBag.Categories = DataHelper.GetCategories();
-            return View();
-        }
-        
-        [HttpPost]
-        public ActionResult CreateCompetition(CompetitionModel model)
-        {
-            DataHelper.AddNewCompetition(model);
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult EditCompetition(int id)
-        {
-			ViewBag.Categories = DataHelper.GetCategories();
-            return View(DataHelper.GetCompetition(id));
-        }
+			return View();
+		}
 
 		[HttpPost]
-        public ActionResult EditCompetition(CompetitionModel model, int id)
-        {
-            DataHelper.EditCompetition(id, model);
-            return RedirectToAction("Index");
-        }
-    }
+		public ActionResult CreateCompetition(CompetitionModel model)
+		{
+			DataHelper.AddNewCompetition(model);
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult EditCompetition(int id)
+		{
+			ViewBag.Categories = DataHelper.GetCategories();
+			return View(DataHelper.GetCompetition(id));
+		}
+
+		[HttpPost]
+		public ActionResult EditCompetition(CompetitionModel model, int id)
+		{
+			DataHelper.EditCompetition(id, model);
+			return RedirectToAction("Index");
+		}
+	}
 }
