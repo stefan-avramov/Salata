d55568b42b92c857b5c167cc5f9465cb966d90eb
Stefan Avramov <st3f.1p@gmail.com>
2013-03-11 08:50:34 +0200

    missing file

---
+++ PrototypeTopCoder/PrototypeTopCoder/Models/HomeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrototypeTopCoder.Models
+{
+
+	public class HomeModel
+	{
+		[Display(Name = "Competitions")]
+		public IList<CompetitionModel> Competitions { get; set; }
+
+		[Display(Name = "Enrolled Compettions")]
+		public HashSet<int> EnrolledCompetitions { get; set; }
+	}
+}
