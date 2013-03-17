36584effa1dbc234350219cd372fad0ada13a48b
unknown <ivan.vergiliev@gmail.com>
2013-03-17 22:14:47 +0200

    Integrated creating simple questions with TinyMCE and FineUploader for uploading photos

---
+++ PrototypeTopCoder/PrototypeTopCoder/Controllers/UploadController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FineUploader
+{
+    public class UploadController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public FineUploaderResult UploadFile(FineUpload upload)
+        {
+            var dir = HttpContext.Server.MapPath("~/App_Data");
+            var fileName = Guid.NewGuid().ToString("N");
+            var filePath = Path.Combine(dir, fileName);
+
+            try
+            {
+                var image = Image.FromStream(upload.InputStream);
+                image.Save(filePath, ImageFormat.Png);
+            }
+            catch (Exception ex)
+            {
+                return new FineUploaderResult(false, error: ex.Message);
+            }
+
+            return new FineUploaderResult(true, new { fileName = fileName });
+        }
+
+        public ActionResult GetImage(String fileName)
+        {
+            var dir = HttpContext.Server.MapPath("~/App_Data/");
+            var path = Path.Combine(dir, fileName);
+            return base.File(path, "image/png");
+        }
+    }
+}
---
+++ PrototypeTopCoder/PrototypeTopCoder/Scripts/custom/TinyMCE.js
@@ -0,0 +1,36 @@
+﻿var fileBrowser = function (fieldName, url, type, win) {
+    var cmsURL = '@Url.Action("Index", "Upload")';
+    if (cmsURL.indexOf("?") < 0) {
+        //add the type as the only query parameter
+        cmsURL = cmsURL + "?type=" + type;
+    }
+    else {
+        //add the type as an additional query parameter
+        // (PHP session ID is now included if there is one at all)
+        cmsURL = cmsURL + "&type=" + type;
+    }
+
+    tinyMCE.activeEditor.windowManager.open({
+        file: cmsURL,
+        title: 'My File Browser',
+        width: 400,  // Your dimensions may differ - toy around with them!
+        height: 300,
+        resizable: "yes",
+        inline: "yes",  // This parameter only has an effect if you use the inlinepopups plugin!
+        close_previous: "no"
+    }, {
+        window: win,
+        input: fieldName
+    });
+    return false;
+}
+
+tinyMCE.init({
+    mode: 'textareas', file_browser_callback: fileBrowser, width: 400, height: 400, theme: 'advanced',
+    theme_advanced_toolbar_location: "top",
+    theme_advanced_toolbar_align: "left",
+    theme_advanced_statusbar_location: "bottom",
+    theme_advanced_resizing: true,
+    plugins: 'inlinepopups',
+    encoding: 'xml'
+});
